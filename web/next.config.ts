1e4b7a0521f181698a2218aa070f997df57dd6f1
testaco <testac+github@gmail.com>
2025-03-11 20:53:29 -0700

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/percy',
   // Disable image optimization since we're doing static export
   images: {
     unoptimized: true,
